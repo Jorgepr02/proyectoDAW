87de929061378c9c6e5b0ba6e87b94238da7937a
Jorgepr02 <jorgepampinrios02@liceolapaz.net>
2025-05-15 02:17:08 +0200

    segundo commit

--- edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/Application.java
+++ edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/Application.java
@@ -2,12 +2,16 @@ package edu.jorge.proyectodaw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import edu.jorge.proyectodaw.repositories.CategoryRepo;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		CategoryRepo categoryRepo = context.getBean(CategoryRepo.class);
 	}
 
 }
