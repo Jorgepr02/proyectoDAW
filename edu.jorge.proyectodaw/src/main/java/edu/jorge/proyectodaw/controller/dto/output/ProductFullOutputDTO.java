21470ff386bc8ef628a8fd1bcd0cbdd89e01d734
Jorgepr02 <jorgepampinrios02@liceolapaz.net>
2025-05-26 23:14:20 +0200

    Crear productos con categorías y features estáticas

---
+++ edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/controller/dto/output/ProductFullOutputDTO.java
@@ -0,0 +1,20 @@
+package edu.jorge.proyectodaw.controller.dto.output;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductFullOutputDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+    private String categoryName;
+    private String categoryType;
+}
