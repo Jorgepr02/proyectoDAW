4d9c5afea3c91aa6b367eb80ee5267b24d780c24
Jorgepr02 <jorgepampinrios02@liceolapaz.net>
2025-05-26 23:11:29 +0200

    Crear productos con categorías y features estáticas

--- edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/controller/dto/output/ProductSimpleOutputDTO.java
+++ edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/controller/dto/output/ProductSimpleOutputDTO.java
@@ -10,11 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductSimpleOutputDTO {
-    private Long id;
     private String name;
-    private String description;
     private Double price;
-    private Integer stock;
-    private String categoryName;
     private String categoryType;
 }
--- edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/service/ProductServiceImp.java
+++ edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/service/ProductServiceImp.java
@@ -1,20 +1,25 @@
 package edu.jorge.proyectodaw.service;
 
-import java.util.List;
-
+import edu.jorge.proyectodaw.controller.dto.input.ProductCreateInputDTO;
+import edu.jorge.proyectodaw.entity.Feature;
+import edu.jorge.proyectodaw.entity.Product;
+import edu.jorge.proyectodaw.entity.ProductFeature;
+import edu.jorge.proyectodaw.repositories.ProductFeatureRepo;
+import edu.jorge.proyectodaw.repositories.ProductRepo;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import edu.jorge.proyectodaw.entity.Product;
-import edu.jorge.proyectodaw.repositories.ProductRepo;
-import jakarta.persistence.EntityNotFoundException;
-import edu.jorge.proyectodaw.enums.CategoryType;
+import java.util.List;
 
 @Service
 public class ProductServiceImp implements ProductService {
 
     @Autowired
     private ProductRepo productRepo;
+    @Autowired
+    private ProductFeatureRepo productFeatureRepo;
     
     @Autowired
     private CategoryService categoryService;
@@ -32,36 +37,45 @@ public class ProductServiceImp implements ProductService {
                 .orElseThrow(() -> new EntityNotFoundException("Producto no encontrado con id: " + id));
     }
 
+    // TODO ESTO ES UN EJEMPLO ¡¡NO SE UTILIZA!!
     @Override
-    public Product save(Product product) {
-        if (product.getCategory() != null && product.getCategory().getId() != null) {
-            product.setCategory(categoryService.findById(product.getCategory().getId()));
-        }
-
-        if (product.getProductFeatures() != null) {
-            for (var feature : product.getProductFeatures()) {
-                if (feature.getFeature() != null && feature.getFeature().getId() != null) {
-                    feature.setFeature(featureService.findById(feature.getFeature().getId()));
-                }
-            }
-            
-        }
-
-        switch (product.getCategory().getCategoryType()) {
-            case CategoryType.SNOWBOARD:
-                // TODO featureRepo.findById(1L)
-                break;
-            case CategoryType.SKI:
-                
-                break;
-        
-            default:
-                break;
-        }
-
-        product.getProductFeatures();
-        return productRepo.save(product);
-    }
+    public Product save(Product product) { return null; }
+//    @Override
+//    public Product save(Product product) {
+//        product.setStock(0);
+//
+//        if (product.getCategory() != null && product.getCategory().getId() != null) {
+//            product.setCategory(categoryService.findById(product.getCategory().getId()));
+//        }
+//
+//        if (product.getProductFeatures() != null) {
+//            for (var feature : product.getProductFeatures()) {
+//                if (feature.getFeature() != null && feature.getFeature().getId() != null) {
+//                    feature.setFeature(featureService.findById(feature.getFeature().getId()));
+//                }
+//            }
+//
+//        }
+//
+//        if (product.getCategory().getCategoryType() == CategoryType.ACCESORY) {
+//            product.setProductFeatures(null);
+//        } else {
+//            if (product.getProductFeatures() == null || product.getProductFeatures().isEmpty()) {
+//                throw new IllegalArgumentException("Los productos de tipo " + product.getCategory().getCategoryType() + " deben tener al menos una característica.");
+//            }
+//
+//            List<Feature> features = featureService.findAll();
+//            List<ProductFeature> productFeatures = new ArrayList<>();
+//            for (Feature feature : features) {
+//                productFeatures.add(
+//                        new ProductFeature(null, product, feature, null)
+//                );
+//            }
+//            product.setProductFeatures(productFeatures);
+//        }
+//
+//        return productRepo.save(product);
+//    }
 
     @Override
     public Product update(Long id, Product product) {
@@ -78,5 +92,34 @@ public class ProductServiceImp implements ProductService {
     public void delete(Long id) {
         Product product = findById(id);
         productRepo.delete(product);
+    }
+
+    // TODO ESTE SI SE ESTÁ UTILIZANDO
+    @Override
+    @Transactional
+    public Product save(ProductCreateInputDTO productCreateInputDTO) {
+
+        Product productToSave = new Product();
+        productToSave.setName(productCreateInputDTO.getName());
+        productToSave.setDescription(productCreateInputDTO.getDescription());
+        productToSave.setPrice(productCreateInputDTO.getPrice());
+        productToSave.setCategory(categoryService.findById(productCreateInputDTO.getIdCategory()));
+        productToSave.setStock(0); // Inicializamos el stock a 0
+
+        Product productCreated = productRepo.saveAndFlush(productToSave); // Se podría omitir, por Transación SQL
+
+//        List<ProductFeature> productFeatures = new ArrayList<>();
+        for (var featureInput : productCreateInputDTO.getProductFeatures()) {
+            Feature feature = featureService.findById(featureInput.getIdFeature());
+            ProductFeature productFeature = new ProductFeature(null, productCreated, feature, featureInput.getValue());
+
+            productFeatureRepo.save(productFeature);
+//            productFeatures.add(productFeature);
+        }
+
+//        productToSave.setProductFeatures(productFeatures);
+//        Product productCreated = productRepo.saveAndFlush(productToSave);
+
+        return productCreated;
     }
 }
