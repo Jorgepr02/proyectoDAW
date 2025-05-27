b88f3a27bd24c83b5fde9c034c9e462c427393a2
Jorgepr02 <jorgepampinrios02@liceolapaz.net>
2025-05-27 20:59:30 +0200

    listar características

--- edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/service/FeatureService.java
+++ edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/service/FeatureService.java
@@ -1,12 +1,10 @@
 package edu.jorge.proyectodaw.service;
 
-import java.util.List;
 import edu.jorge.proyectodaw.entity.Feature;
+
+import java.util.List;
 
 public interface FeatureService {
     List<Feature> findAll();
     Feature findById(Long id);
-    Feature save(Feature feature);
-    Feature update(Long id, Feature feature);
-    void delete(Long id);
 }
--- edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/service/FeatureServiceImp.java
+++ edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/service/FeatureServiceImp.java
@@ -1,13 +1,12 @@
 package edu.jorge.proyectodaw.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import edu.jorge.proyectodaw.entity.Feature;
 import edu.jorge.proyectodaw.repositories.FeatureRepo;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FeatureServiceImp implements FeatureService {
@@ -24,23 +23,5 @@ public class FeatureServiceImp implements FeatureService {
     public Feature findById(Long id) {
         return featureRepo.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Característica no encontrada con id: " + id));
-    }
-
-    @Override
-    public Feature save(Feature feature) {
-        return featureRepo.save(feature);
-    }
-
-    @Override
-    public Feature update(Long id, Feature feature) {
-        Feature existingFeature = findById(id);
-        existingFeature.setName(feature.getName());
-        return featureRepo.save(existingFeature);
-    }
-
-    @Override
-    public void delete(Long id) {
-        Feature feature = findById(id);
-        featureRepo.delete(feature);
     }
 }
