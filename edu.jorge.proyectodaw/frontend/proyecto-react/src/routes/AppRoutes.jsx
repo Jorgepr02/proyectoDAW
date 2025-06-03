c0e1d36d948176c18914cea5a06eebc109550479
Jorgepr02 <jpr03615@gmail.com>
2025-06-04 00:43:31 +0200

    Pagina de FAQ y corrección de errores

--- edu.jorge.proyectodaw/frontend/proyecto-react/src/routes/AppRoutes.jsx
+++ edu.jorge.proyectodaw/frontend/proyecto-react/src/routes/AppRoutes.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import Footer from "../components/Footer/Footer";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import HomePage from "../pages/HomePage";
 import ProductsPage from "../pages/ProductsPage";
 import ProductDetail from "../pages/ProductDetail";
 import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
 import AboutPage from "../pages/AboutPage";
+import FaqPage from "../pages/Faq";
 
 const AppRoutes = () => {
   return (
-    <Router>
-      <div className="App">
+    <BrowserRouter>
+      <div className="app-layout">
         <Header />
-        <main>
+        <main className="main-content">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/productos" element={<ProductsPage />} />
@@ -22,11 +23,12 @@ const AppRoutes = () => {
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/sobre-nosotros" element={<AboutPage />} />
+            <Route path="/faq" element={<FaqPage />} />
           </Routes>
         </main>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 };
 
