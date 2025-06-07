5647037ea118f75edcf9d14c554c74e7b9c844b4
Jorgepr02 <jpr03615@gmail.com>
2025-06-07 19:50:47 +0200

    adapto controlador de categoría

--- edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/controller/CategoryController.java
+++ edu.jorge.proyectodaw/src/main/java/edu/jorge/proyectodaw/controller/CategoryController.java
@@ -1,17 +1,18 @@
 package edu.jorge.proyectodaw.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import edu.jorge.proyectodaw.controller.dto.output.CategoryFullOutputDTO;
 import edu.jorge.proyectodaw.controller.dto.output.CategorySimpleOutputDTO;
 import edu.jorge.proyectodaw.entity.Category;
 import edu.jorge.proyectodaw.service.CategoryService;
-// import jakarta.websocket.server.PathParam; // Not needed, use Spring's @PathVariable
-import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -19,14 +20,54 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryFullOutputDTO>> findAll() {
+        List<CategoryFullOutputDTO> categoryFullOutputDTOList = new ArrayList<>();
+        List<Category> categoryList = categoryService.findAll();
+
+        for (Category category : categoryList) {
+            CategoryFullOutputDTO categoryFullOutputDTO = convertToFullDTO(category);
+            categoryFullOutputDTOList.add(categoryFullOutputDTO);
+        }
+
+        return ResponseEntity.ok(categoryFullOutputDTOList);
+    }
+
     @GetMapping("{id}")
     public ResponseEntity<CategorySimpleOutputDTO> findById(@PathVariable Long id) {
         CategorySimpleOutputDTO categorySimpleOutputDTO = new CategorySimpleOutputDTO();
         Category category = categoryService.findById(id);
         categorySimpleOutputDTO.setName(category.getName());
         categorySimpleOutputDTO.setCategoryType(category.getCategoryType().getDescription());
-        return ResponseEntity.ok(categorySimpleOutputDTO);   
+        return ResponseEntity.ok(categorySimpleOutputDTO);
     }
 
+    private CategorySimpleOutputDTO convertToDTO(Category category) {
+        CategorySimpleOutputDTO dto = new CategorySimpleOutputDTO();
+
+        dto.setName(category.getName());
+        if (category.getCategoryType() != null) {
+            dto.setCategoryType(category.getCategoryType().getDescription());
+        } else {
+            dto.setCategoryType("No category type");
+        }
+
+        return dto;
+    }
+
+    private CategoryFullOutputDTO convertToFullDTO(Category category) {
+        CategoryFullOutputDTO dto = new CategoryFullOutputDTO();
+
+        dto.setId(category.getId());
+        dto.setName(category.getName());
+        if (category.getCategoryType() != null) {
+            dto.setCategoryType(category.getCategoryType().getDescription().toUpperCase());
+        } else {
+            dto.setCategoryType("No category type");
+        }
+
+        return dto;
+    }
 
 }
