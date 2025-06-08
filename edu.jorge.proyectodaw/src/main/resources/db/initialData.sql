85b258eda1dbeb2c7fc4b32675a1541b3ce7376b
Jorgepr02 <jpr03615@gmail.com>
2025-06-08 15:49:11 +0200

    arreglado fallo con category_type de accesorios

--- edu.jorge.proyectodaw/src/main/resources/db/initialData.sql
+++ edu.jorge.proyectodaw/src/main/resources/db/initialData.sql
@@ -2,7 +2,7 @@
 INSERT INTO category (name, description, category_type) VALUES
 ('Snowboard', 'Tablas y equipamiento específico para snowboard', 'SNOWBOARD'),
 ('Ski', 'Esquís y material especializado para esquiar', 'SKI'),
-('Accesorios', 'Complementos y accesorios para deportes de nieve', NULL);
+('Accesorios', 'Complementos y accesorios para deportes de nieve', 'ACCESORY');
 
 -- Feature
 INSERT INTO features (name) VALUES
